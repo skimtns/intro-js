5971a7df23cf67eb4f9cf403fd76a3c6b68b5041
Adriana Vars <asvutah@gmail.com>
2018-12-10 21:01:21 -0700

    notes from codepen

--- app.js
+++ app.js
@@ -82,3 +82,168 @@ var colors = ["red", "#00FF48", "blue", "orange"]
   }
 
   console.log(person.func())
+
+
+  // single comment
+/* 
+multi
+line comment// 
+*/
+
+// array - is an object not a data type 
+// string
+// float
+// integer
+// boolean
+// null 
+// undefined 
+// object key, value pairs
+// date
+
+
+// console.log('hello world today')
+
+// // operator
+// console.log(1+2)
+// console.log(1-1)
+// console.log(1*5)
+// console.log(1/1)
+// console.log(Math.pow(2,3))
+// console.log((2+3*6) -1*5)
+
+// comparators, always returns true or false, > < >= <= == === !=
+
+// console.log( 4 > 2)
+
+// logical operators && (both sides to be true)   || (one side has to be true)
+
+// variables 
+
+// var name = 'Bob'
+// camel case for multiple words variable names 
+// starts with a letter or $ or _ 
+// case sensitive 
+// cannont have them be reserved words 
+// var lastName = 'marley'
+
+// var balance = 1000
+// balance++
+// balance--
+// balance+= 1
+// balance-=10
+// console.log(balance)
+
+// var greeting = "hello"
+// var planet = "world"
+// console.log(greeting + planet)
+
+// console.log(greeting + 32)
+// console.log(greeting +3/2)
+
+// console.log(greeting + " Name: \t World")
+
+// console.log(greeting.length)
+// console.log(greeting.length > planet )
+// console.log(greeting.charAt(4))
+
+// var num = Number("1.363234343")
+// console.log(num)
+// console.log(parseInt('1.86876'))
+// console.log(parseFloat(1.23434))
+// console.log(num.toString())
+
+// console.log(num.toFixed(2))
+// console.log(num.toPrecision(2))
+
+// Math.random()
+// console.log((Math.random() * 7).toPrecision(2))
+// console.log(Math.min(2,1,-1,12,28))
+// console.log(Math.round(3.74))
+// console.log(Math.floor(3.74))
+// console.log(Math.ceil(3.14))
+// console.log(Math.PI)
+
+// var arr = ["Marc", "Kira", "Henry"]
+// console.log(arr[2])
+// console.log(arr.indexOf("Kira"))
+// console.log(arr.join(" and "))
+// console.log(arr.includes("Marc"))
+
+// << ruby way to push objects in the array
+
+// console.log(arr.push("green"))
+// console.log(arr)
+// console.log(arr.unshift("red"))
+// console.log(arr)
+// console.log(arr.pop())
+// console.log(arr)
+// console.log(arr.slice(1))
+// console.log(arr)
+// console.log(arr.length)
+// console.log(arr.reverse())
+// console.log(arr)
+
+// for (var i = 0; i < arr.length; i++) {
+//   console.log(arr[i])
+// }
+
+// arr.forEach (function(element) {
+//   console.log(element)
+// })
+
+// map
+
+// arr.map (function(a){
+//   return a 
+// })
+
+
+// filter
+
+// arr.filter(function (a){
+//   console.log(a)
+// })
+
+// arr.reduce(function(a, num){
+//   return a + num 
+// })
+
+// objects
+
+// - hash key value pairs {}
+// - Json key value pairs 
+// - Instantied Objects
+//   var person = Person.new()
+
+// var person = {name: "bob", age:36}
+// console.log(person.name)
+// console.log(person['name'])
+
+// console.log(person.name = 'craig')
+
+// var people = {
+//   name: ['bob', 'joe', 'susie', 'jane']
+//   ages: [2,3,4,5,6]
+// }
+
+// var cars = {
+//   cars: [
+//     {make: 'ford', price: 12345},
+//     {make: 'fordsasds', price: 125},
+//   ]
+// }
+
+
+//JSON
+
+// Javascript Object Notation
+// {
+//   "firstName": "john",
+//   "lastName": "wayne"
+// }
+
+// var person = JSON.parse(object)
+
+// console.log()
+
+// debugger is the debugger for the js = like pry in ruby
